d5abbc204357032e3593d0ff29be19cc44660a22
Maxim Murphy <maxim.murphy8@gmail.com>
2022-10-06 17:09:33 -0400

    Added Start Adventure button that leads to AdventureActivity and Past Adventures button that leads to PastAdventuresActivity

--- app/src/main/java/com/example/adventureapp/MainActivity.java
+++ app/src/main/java/com/example/adventureapp/MainActivity.java
@@ -9,11 +9,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button settingsButton;
+    private Button startAdventureButton;
+    private Button pastAdventuresButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startAdventureButton = (Button) findViewById(R.id.startAdventureButton);
+        startAdventureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openAdventureActivity();
+            }
+        });
+
+        pastAdventuresButton = (Button) findViewById(R.id.pastAdventuresButton);
+        pastAdventuresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openPastAdventuresActivity();
+            }
+        });
 
         settingsButton = (Button) findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +42,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void openAdventureActivity(){
+        Intent intent = new Intent (this, AdventureActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPastAdventuresActivity(){
+        Intent intent = new Intent (this, PastAdventuresActivity.class);
+        startActivity(intent);
+    }
+
     public void openSettingsActivity(){
         Intent intent = new Intent (this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void onDestroy() {
+
+        super.onDestroy();
+
     }
 }
