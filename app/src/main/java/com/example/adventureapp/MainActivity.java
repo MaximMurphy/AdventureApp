d1a6402d994f2421c1fb4d788d8ba6d93b0ad338
Maxim Murphy <maxim.murphy8@gmail.com>
2022-10-06 16:00:38 -0400

    Added settings button on home page to lead to SettingsActivity

--- app/src/main/java/com/example/adventureapp/MainActivity.java
+++ app/src/main/java/com/example/adventureapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.adventureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openSettingsActivity();
+            }
+        });
+    }
+
+    public void openSettingsActivity(){
+        Intent intent = new Intent (this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
